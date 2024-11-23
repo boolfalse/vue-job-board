8e29d3c2448268a92e869c0bc6c5c287764a2ef7
boolfalse <extravaganza1024@gmail.com>
2024-11-23 10:26:09 -0800

    vue-router | RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 
 <script setup>
 import Navbar from "@/components/Navbar.vue";
-import Hero from "@/components/Hero.vue";
-import HomeCards from "@/components/HomeCards.vue";
-import Items from "@/components/Items.vue";
 </script>
 
 <template>
   <Navbar />
-  <Hero title="Home" subtitle="Welcome to the Home Page" />
-  <HomeCards />
-  <Items :limit="3" showButton />
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+
+<script setup>
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import Items from "@/components/Items.vue";
+</script>
+
+<template>
+  <Hero />
+  <HomeCards />
+  <Items :limit="3" :showButton="true" />
+</template>
